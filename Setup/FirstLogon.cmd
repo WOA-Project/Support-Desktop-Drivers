20e7dada5b24f676690996939e99bba3640b9445
Gus <gus33000@live.fr>
2019-08-10 19:07:52 +0200

    Added Chat app

--- Setup/FirstLogon.cmd
+++ Setup/FirstLogon.cmd
@@ -1,2 +1,7 @@
 @echo off
+
+REM System apps
+dism.exe /Online /Add-ProvisioningPackage /PackagePath:%SystemDrive%\Windows\Provisioning\Packages\SPApps.ppkg
+powershell -ExecutionPolicy Bypass -File \Windows\OEM\SPApps.ps1
+
 cmd.exe /c \Windows\OEM\IHVSettingsFirstLogon.cmd
