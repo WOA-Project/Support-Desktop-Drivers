5f333b01c8c9179b0b555e71ec7b7301458be435
Gus <gus33000@live.fr>
2019-08-12 08:58:07 +0200

    Nulled secondary broadband adapter for single sim devices

--- Setup/Oobe.cmd
+++ Setup/Oobe.cmd
@@ -1,8 +1,14 @@
 @echo off
 
 REM Second boot drivers
-\Windows\OEM\devcon.exe update \Windows\OEM\Drivers\ChargeArbitration.inf Root\CAD
 \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\qcmbb.wp8994.inf QCMS\QCOM0EA0
+\Windows\OEM\devcon.exe update \Windows\OEM\Drivers\ChargeArbitration.inf Root\CAD
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\qcxhcifilter*.inf') do \Windows\OEM\devcon.exe update %%f URS\QCOM24B6^&HOST
+
+\Windows\OEM\devcon status @QCMS\QCOM0EA0\*1 | findstr "43" > nul
+if %errorlevel%==0 \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\qcmbbnull.inf @QCMS\QCOM0EA0\*1
+
+\Windows\OEM\devcon status @QCMS\QCOM0EA0\*0 | findstr "43" > nul
+if %errorlevel%==0 \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\qcmbbnull.inf @QCMS\QCOM0EA0\*0
 
 cmd.exe /c \Windows\OEM\IHVDriversOobe.cmd
