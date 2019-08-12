fffd0ed1dbfbc52abb7053affe3e76d22ea77a00
Gus <gus33000@live.fr>
2019-08-12 11:08:21 +0200

    Fixed a path issue with the null script

--- Setup/Oobe.cmd
+++ Setup/Oobe.cmd
@@ -1,22 +1,22 @@
 @echo off
 
 REM Second boot drivers
-\Windows\OEM\devcon.exe update \Windows\OEM\Drivers\qcmbb.wp8994.inf QCMS\QCOM0EA0
 \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\ChargeArbitration.inf Root\CAD
+\Windows\OEM\devcon.exe update \Windows\OEM\Drivers\qcmbb.wp8994.inf QCMS\QCOM0EA0
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\qcxhcifilter*.inf') do \Windows\OEM\devcon.exe update %%f URS\QCOM24B6^&HOST
 
-devcon status @QCMS\QCOM0EA0\*1 | findstr "43" > nul
+cmd.exe /c \Windows\OEM\IHVDriversOobe.cmd
+
+\Windows\OEM\devcon.exe status @QCMS\QCOM0EA0\*1 | findstr "43" > nul
 if %errorlevel%==0 (
     \Windows\OEM\devcon.exe remove @QCMS\QCOM0EA0\*1
     \Windows\OEM\devcon.exe dp_add \Windows\OEM\Drivers\qcmbbnull.inf
     \Windows\OEM\devcon.exe rescan
 )
 
-devcon status @QCMS\QCOM0EA0\*0 | findstr "43" > nul
+\Windows\OEM\devcon.exe status @QCMS\QCOM0EA0\*0 | findstr "43" > nul
 if %errorlevel%==0 (
     \Windows\OEM\devcon.exe remove @QCMS\QCOM0EA0\*0
     \Windows\OEM\devcon.exe dp_add \Windows\OEM\Drivers\qcmbbnull.inf
     \Windows\OEM\devcon.exe rescan
 )
-
-cmd.exe /c \Windows\OEM\IHVDriversOobe.cmd
