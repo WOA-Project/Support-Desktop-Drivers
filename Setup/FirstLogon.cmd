68962036b7640ba757a9328981c8ed5653b97a21
Gus <gus33000@live.fr>
2019-08-12 08:35:47 +0200

    Re-ordered files in the OEM folder

--- Setup/FirstLogon.cmd
+++ Setup/FirstLogon.cmd
@@ -2,6 +2,6 @@
 
 REM System apps
 dism.exe /Online /Add-ProvisioningPackage /PackagePath:%SystemDrive%\Windows\Provisioning\Packages\SPApps.ppkg
-powershell -ExecutionPolicy Bypass -File \Windows\OEM\SPApps.ps1
+powershell -ExecutionPolicy Bypass -File \Windows\OEM\Applications\SPApps.ps1
 
 cmd.exe /c \Windows\OEM\IHVSettingsFirstLogon.cmd
--- Setup/SpecializeCommands.cmd
+++ Setup/SpecializeCommands.cmd
@@ -1,5 +1,5 @@
 @echo off
 cmd.exe /c \Windows\OEM\IHVSettingsSpecialize.cmd
 
-\Windows\OEM\devcon.exe update \Windows\OEM\qcaud8994.inf AUDD\QCOM2451
+\Windows\OEM\devcon.exe update \Windows\OEM\Drivers\qcaud8994.inf AUDD\QCOM2451
 cmd.exe /c \Windows\OEM\IHVDriversSpecialize.cmd
