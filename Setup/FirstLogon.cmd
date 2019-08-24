5b4ae066f895f15ac8e4c044a4e2fa667a0f06f2
Gus <gus33000@live.fr>
2019-08-24 12:12:30 +0200

    Added status indications for application setup

--- Setup/FirstLogon.cmd
+++ Setup/FirstLogon.cmd
@@ -1,7 +1,11 @@
 @echo off
 
+\Windows\OEM\SilentProvisionerT.exe
+
 REM System apps
 dism.exe /Online /Add-ProvisioningPackage /PackagePath:%SystemDrive%\Windows\Provisioning\Packages\SPApps.ppkg
 powershell -ExecutionPolicy Bypass -File \Windows\OEM\Applications\SPApps.ps1
 
 cmd.exe /c \Windows\OEM\IHVSettingsFirstLogon.cmd
+
+\Windows\OEM\SilentProvisionerT.exe 1
