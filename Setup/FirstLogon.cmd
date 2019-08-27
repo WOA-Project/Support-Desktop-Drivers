d797ea865877bf0f2efa4088bb37e247617f36e1
Gus <gus33000@live.fr>
2019-08-27 13:53:24 +0200

    Updates

--- Setup/FirstLogon.cmd
+++ Setup/FirstLogon.cmd
@@ -3,7 +3,6 @@
 \Windows\OEM\SilentProvisionerT.exe
 
 REM System apps
-dism.exe /Online /Add-ProvisioningPackage /PackagePath:%SystemDrive%\Windows\Provisioning\Packages\SPApps.ppkg
 powershell -ExecutionPolicy Bypass -File \Windows\OEM\Applications\SPApps.ps1
 
 cmd.exe /c \Windows\OEM\IHVSettingsFirstLogon.cmd
