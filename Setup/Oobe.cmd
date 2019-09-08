273ef0099948a5a144473753f0077ed2bee4bbd4
Gus <gus33000@live.fr>
2019-09-08 08:20:05 +0200

    New installation method for root devices and added auto brightness service

--- Setup/Oobe.cmd
+++ Setup/Oobe.cmd
@@ -2,7 +2,7 @@
 
 REM Second boot drivers
 \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\ChargeArbitration.inf Root\CAD
-for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\fusionv2.inf') do \Windows\OEM\devcon.exe install %%f umdf2\FusionV2
+call :installRootDevice fusionv2.inf umdf2\FusionV2 ROOT\FusionV2\0000
 \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\qcmbb.wp8994.inf QCMS\QCOM0EA0
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\qcxhcifilter*.inf') do \Windows\OEM\devcon.exe update %%f URS\QCOM24B6^&HOST
 
@@ -21,3 +21,18 @@ if %errorlevel%==0 (
     \Windows\OEM\devcon.exe dp_add \Windows\OEM\Drivers\qcmbbnull.inf
     \Windows\OEM\devcon.exe rescan
 )
+
+goto :eof
+
+REM 
+REM Arguments:
+REM 
+REM 1: Driver Inf filename in driver store's file repository
+REM 2: Driver Inf hardware id
+REM 3: Root driver hardware id
+REM 
+:installRootDevice 
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\%1') do \Windows\OEM\devcon.exe install %%f %3
+\Windows\OEM\devcon.exe sethwid @%3 := +%2
+for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\%1') do \Windows\OEM\devcon.exe update %%f %3
+goto :eof
