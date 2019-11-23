9a9b69f219e3e6418ebab2a7c43ff3b944273f0d
Gustave Monce <gustave.monce@outlook.com>
2019-11-23 17:46:02 +0100

    Updated driver setup

--- Setup/Oobe.cmd
+++ Setup/Oobe.cmd
@@ -3,7 +3,18 @@
 REM Second boot drivers
 \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\ChargeArbitration.inf Root\CAD
 call :installRootDevice fusionv2.inf umdf2\FusionV2 ROOT\FusionV2\0000
-\Windows\OEM\devcon.exe update \Windows\OEM\Drivers\qcmbb.wp8994.inf QCMS\QCOM0EA0
+
+REM MBB
+powershell -command "[System.Environment]::OSVersion.Version.Build" > psout.txt
+set /p osbuild=<psout.txt
+del psout.txt
+
+if %osbuild% gtr 18908 (
+    \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\WA\qcmbb.wp8994.inf QCMS\QCOM0EA0
+) else (
+    \Windows\OEM\devcon.exe update \Windows\OEM\Drivers\WP\qcmbb.wp8994.inf QCMS\QCOM0EA0
+)
+
 for /f "delims=*" %%f in ('dir /b /s \Windows\System32\DriverStore\FileRepository\qcxhcifilter*.inf') do \Windows\OEM\devcon.exe update %%f URS\QCOM24B6^&HOST
 
 cmd.exe /c \Windows\OEM\IHVDriversOobe.cmd
